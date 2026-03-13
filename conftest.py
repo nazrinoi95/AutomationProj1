f7b2993f070b3fd87479186982bd28fe541c3213
nazrin_PC <nazrinoi.work@gmail.com>
2026-03-14 07:34:37 +0800

    Added some comments to understand

--- conftest.py
+++ conftest.py
@@ -1,15 +1,25 @@
-import pytest
-from core.driver import get_driver
+import pytest #This imports the pytest library
+from core.driver import get_driver #This imports from core file with driver fn and import the get_driver method
 
-def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="chrome")
-    parser.addoption("--headless", action="store_true")
+def pytest_addoption(parser): #method
+    parser.addoption("--browser", action="store", default="chrome") #This stores browser = chrome, that is what action does
+    parser.addoption("--headless", action="store_true") #This ones stores if true, eg: --headless = Y , --headless = true
+    #Why do we use the parser.adoption method or class idk?
 
-@pytest.fixture
-def driver(request):
-    browser = request.config.getoption("--browser")
-    headless = request.config.getoption("--headless")
+@pytest.fixture #This provide resources for test : setup → give resource → cleanup. Resource now is SWD?
+def driver(request):#method
+    browser = request.config.getoption("--browser") #This read the value from command line and store it
+    headless = request.config.getoption("--headless") #This read the value from command line and store it
 
-    driver = get_driver(browser=browser, headless=headless)
-    yield driver # Give driver to test here
+    driver = get_driver(browser=browser, headless=headless) #Calls the driver.py
+    yield driver # This one for cleanup
     driver.quit()
+
+    #Flow would be
+    # driver created
+    # ↓
+    # yield driver
+    # ↓
+    # test uses driver
+    # ↓
+    # driver.quit()
--- core/driver.py
+++ core/driver.py
@@ -1,21 +1,20 @@
-from requests import options
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.chrome.options import Options as ChromeOptions #Import and rename
+from selenium.webdriver.firefox.options import Options as FirefoxOptions #Import and rename
 
 
-def get_driver(browser = "chrome", headless=False):
-    if browser.lower() == "chrome":
+def get_driver(browser = "chrome", headless=False): #This one set the default value as chrome and headless false
+    if browser.lower() == "chrome": #Lower used so that it's not case-sensitive
         options = ChromeOptions()
         if headless:
-            options.add_argument("--headless=new")
-        options.add_argument("--start-maximized")
-        driver = webdriver.Chrome(options=options)
+            options.add_argument("--headless=new") # Chrome have this flag
+        options.add_argument("--start-maximized") #Starts with max screen
+        driver = webdriver.Chrome(options=options)#This pass options we created in to webdriver.Chrome(options..
     elif browser.lower() == "firefox":
         options = FirefoxOptions()
         if headless:
-            options.add_argument("--headless")
-        options.add_argument("--start-maximised")
+            options.add_argument("--headless") #Firefox only have old flag
+        options.add_argument("--start-maximized")
         driver = webdriver.Firefox(options=options)
     else:
         raise ValueError(f"Browser not supported: {browser}")
